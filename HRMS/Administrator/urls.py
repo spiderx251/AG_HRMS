99828990a3da09da003d4a39dd1d00f123643c4a
GouthamJagarapu <jagarapugoutham@gmail.com>
2023-11-10 00:44:26 +0530

    add a new employee

--- HRMS/Administrator/urls.py
+++ HRMS/Administrator/urls.py
@@ -20,9 +20,9 @@ from Administrator  import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('all_emp', views.view_all_emp, name='view_all_emp'),
-    path('add_emp', views.add_emp, name='add_emp'),
-    path('remove_emp', views.remove_emp, name='remove_emp'),
-    path('filter_emp', views.filter_emp, name='filter_emp'),
+    path('all_emp/', views.view_all_emp, name='view_all_emp'),
+    path('add_emp/', views.add_emp, name='add_emp'),
+    path('remove_emp/', views.remove_emp, name='remove_emp'),
+    path('filter_emp/', views.filter_emp, name='filter_emp'),
     
 ]
