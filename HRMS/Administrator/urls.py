d885d1c79c6fee3aa40dc5771fcc4bae21a12558
GouthamJagarapu <jagarapugoutham@gmail.com>
2023-11-10 23:16:48 +0530

    In Administrator App, Remove and employee and filter employees

--- HRMS/Administrator/urls.py
+++ HRMS/Administrator/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('all_emp/', views.view_all_emp, name='view_all_emp'),
     path('add_emp/', views.add_emp, name='add_emp'),
     path('remove_emp/', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:employee_id>', views.remove_emp, name='remove_emp'),
     path('filter_emp/', views.filter_emp, name='filter_emp'),
     
 ]
